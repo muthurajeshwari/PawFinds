2f427b71bbc53dc7c5271f49ecde28bed53a3ffb
Vasanthan <mvasan531@apac.comcast.com>
2025-05-16 12:09:03 +0530

    Feat:Implement filtering of pets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from './Components/Navbar/Navbar.jsx';
+import Navbar from './Components/Navbar/navbar.jsx';
 import Home from './Components/Home/home.jsx';
 import About from './Components/About/about.jsx';
 import Contact from './Components/Contact/contact.jsx';
 import Pets from './Components/Pets/pets.jsx';
 import Footer from './Components/Footer/footer.jsx';
+import Login from './Components/Login/login.jsx';
+import Adoption from './Components/Adoption/adoption.jsx';
+
 
 function App() {
   return (
@@ -14,12 +17,23 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<><Home /> <Footer/></>} />
-        <Route path="/about" element={<><About /> <Footer/></>} />
-        <Route path="/contact" element={<><Contact /> <Footer/></>} />
-        <Route path="/pets" element={<><Pets /> <Footer/></>} />
-
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pets" element={<Pets />} />
+         <Route path="/login" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <div style={{ textAlign: 'center', marginTop: '50px' }}>
+                <h1>404 - Page Not Found</h1>
+                <p>The page you're looking for doesn't exist.</p>
+              </div>
+            }
+          />
+              <Route path="/adoption" element={<Adoption />} />
       </Routes>
+      <Footer/>
     </Router>
     </>
   );
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -17,18 +17,18 @@ const Contact = () => {
           />
         </div>
         <div className="right">
-          <form action="">
+          <form action="https://getform.io/f/bvryelvb" method="POST">
             <label htmlFor="name">
               Enter your Name:
-              <input type="text" />
+              <input type="text" name="name" />
             </label>
             <label htmlFor="email">
               Enter your mail:
-              <input type="email" />
+              <input type="email" name="email" />
             </label>
             <label htmlFor="msg">
               Enter your Question:
-              <textarea name="msg" id=""></textarea>
+              <textarea name="msg"></textarea>
             </label>
 
             <button className="btn">Send</button>
